44a953b820a3efcb49e5f489b16ea3140a76ca8f
EliaAR <alonsoramos.elia@gmail.com>
2021-07-26 18:17:56 +0200

    fix bug on production

--- src/components/Characters/CharacterPage.jsx
+++ src/components/Characters/CharacterPage.jsx
@@ -9,22 +9,28 @@ import { CharacterList } from "./CharacterList";
 import { Footer } from "../Common/Footer";
 import "./characterPage.scss";
 
-const defaultData = GetLocalStorage("characterArray", []);
-const defaultSearchValueName = GetLocalStorage("searchValueName", "");
-const defaultSelectSpecies = GetLocalStorage("selectSpecies", "");
-const defaultSelectStatus = GetLocalStorage("selectStatus", "");
-const defaultPages = GetLocalStorage("pages", 1);
-const defaultCurrentPage = GetLocalStorage("currentPage", 1);
+// const defaultData = GetLocalStorage("characterArray", []);
+// const defaultSearchValueName = GetLocalStorage("searchValueName", "");
+// const defaultSelectSpecies = GetLocalStorage("selectSpecies", "");
+// const defaultSelectStatus = GetLocalStorage("selectStatus", "");
+// const defaultPages = GetLocalStorage("pages", 1);
+// const defaultCurrentPage = GetLocalStorage("currentPage", 1);
 
 function CharacterPage() {
-  const [data, setData] = useState(defaultData);
+  const [data, setData] = useState(GetLocalStorage("characterArray", []));
   const [searchValueName, setSearchValueName] = useState(
-    defaultSearchValueName
+    GetLocalStorage("searchValueName", "")
   );
-  const [selectSpecies, setSelectSpecies] = useState(defaultSelectSpecies);
-  const [selectStatus, setSelectStatus] = useState(defaultSelectStatus);
-  const [pages, setPages] = useState(defaultPages);
-  const [currentPage, setCurrentPage] = useState(defaultCurrentPage);
+  const [selectSpecies, setSelectSpecies] = useState(
+    GetLocalStorage("selectSpecies", "")
+  );
+  const [selectStatus, setSelectStatus] = useState(
+    GetLocalStorage("selectStatus", "")
+  );
+  const [pages, setPages] = useState(GetLocalStorage("pages", 1));
+  const [currentPage, setCurrentPage] = useState(
+    GetLocalStorage("currentPage", 1)
+  );
   const [error, setError] = useState(false);
 
   useEffect(() => {
