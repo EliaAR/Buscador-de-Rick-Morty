ccd3e0944a847a328f533cb1517e7d3a6e929d5d
EliaAR <alonsoramos.elia@gmail.com>
2021-08-27 19:39:41 +0200

    create burguer button

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,11 +1,40 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import Title from "../../images/Title.png";
 import "./header.scss";
 
 function Header() {
+  const [burguer, setBurguer] = useState(false);
+
   return (
     <header className="header">
-      <p className="header__paragraph">El Mejorado Buscador de </p>
+      <button
+        onClick={() => setBurguer(!burguer)}
+        className="header__menuContainer"
+      >
+        <nav className="header__menuNav fas fa-bars">
+          {burguer ? (
+            <ul className="header__menuUl">
+              <li>
+                <Link to="/characterPage" className="header__menuLink">
+                  Buscador de personajes
+                </Link>
+              </li>
+              <li>
+                <Link to="/locationPage" className="header__menuLink">
+                  Buscador de localizaciones
+                </Link>
+              </li>
+              <li>
+                <Link to="/episodePage" className="header__menuLink">
+                  Buscador de episodios
+                </Link>
+              </li>
+            </ul>
+          ) : null}
+        </nav>
+      </button>
+      <p className="header__paragraph">El Mejorado Buscador de</p>
       <Link to="/">
         <img className="header__img" src={Title} alt="title serie" />
       </Link>
